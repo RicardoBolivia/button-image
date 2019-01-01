dcd14ab9f68cbdbd05daab676db750241bc9dacf
Ricardo <ricardozapata@ieee.org>
2019-01-01 17:03:45 -0400

    Espero que este aporte sea de utilidad

---
+++ buttonImage.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace imageButton
+{
+	partial class buttonImage
+	{
+		/// <summary> 
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de componentes
+
+		/// <summary> 
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// buttonImage
+			// 
+			this.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.MouseLeave += new System.EventHandler(this.buttonImage_MouseLeave);
+			this.MouseHover += new System.EventHandler(this.buttonImage_MouseHover);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ buttonImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace imageButton
+{
+	public partial class buttonImage : Button
+	{
+		public buttonImage()
+		{
+			InitializeComponent();
+		}
+		private Image imageHover;
+
+		private Image imageNormal;
+
+		public Image NormalImage
+		{
+			get { return imageNormal; }
+			set { imageNormal = value; }
+		}
+		
+		public Image HoverImage
+		{
+			get { return imageHover; }
+			set { imageHover = value; }
+		}
+
+		private void buttonImage_MouseHover(object sender, System.EventArgs e)
+		{
+			this.Image = imageHover;
+		}
+
+		private void buttonImage_MouseLeave(object sender, System.EventArgs e)
+		{
+			this.Image = imageNormal;
+		}
+	}
+}
